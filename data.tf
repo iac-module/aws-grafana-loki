522622845dfa62a7a80fca6dc152e726bddc87a6
Oleksandr Matkovskyi <matkovskiy.aleksandrATgmailDOTcom>
2024-09-16 13:27:56 +0300

    D-0001: init version

---
+++ data.tf
@@ -0,0 +1,20 @@
+data "aws_caller_identity" "current" {}
+data "aws_region" "current" {}
+
+data "aws_iam_policy_document" "s3" {
+  statement {
+    actions = [
+      "s3:PutObject",
+      "s3:GetObject",
+      "s3:DeleteObject"
+    ]
+    resources = ["${module.s3_bucket.s3_bucket_arn}/*"]
+  }
+  statement {
+    actions = [
+      "s3:ListBucket"
+    ]
+    resources = [module.s3_bucket.s3_bucket_arn]
+  }
+
+}
